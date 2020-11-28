91fc56fcba385f69ca4194e6a13fe9d2b3016fe9
Saikat Das <saikatdas0790@gmail.com>
2020-11-28 08:46:51 +0530

    initial commit

---
+++ src/routes/dashboard.svelte
@@ -0,0 +1,8 @@
+<script lang="ts" context="module">
+  export async function preload(page, session) {
+    let { user } = session;
+    if (!user) {
+      return this.redirect(302, "/login");
+    }
+  }
+</script>
